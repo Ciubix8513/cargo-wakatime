75f7c08b297bcdb8c3a0ba7d55c549ddb6a8be8a
Luna <ciubix8514@gmail.com>
2023-03-20 10:51:29 +0300

    Finished get_key,ask_for_a_key functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-use std::{env, fs, io::Read};
+use std::{
+    env, fs,
+    io::{self, Read, Write},
+};
 
-fn ask_for_a_key() -> Option<String> {
-    todo!();
+fn ask_for_a_key(prompt: Option<&str>) -> Option<String> {
+    if let Some(prompt) = prompt {
+        println!("{}", prompt);
+    }
+    println!("Please enter your api key");
+    let mut input = String::new();
+    let stdin = io::stdin();
+
+    if stdin.read_line(&mut input).is_err() {
+        return None;
+    }
+    //Add potential key validation here
+    return Some(input);
 }
 
 fn get_key() -> Option<String> {
@@ -9,9 +23,10 @@ fn get_key() -> Option<String> {
     let data_path = binding.to_str()?;
     let data_path = String::from(data_path) + "cargo-wakatime/";
     println!("{}", data_path);
-    fs::create_dir(data_path.clone()).unwrap();
+    fs::create_dir(data_path.clone());
+
     let data_path = data_path + "key";
-    let file = fs::OpenOptions::new().read(true).open(data_path);
+    let file = fs::OpenOptions::new().read(true).open(&data_path);
     match file {
         Ok(mut f) => {
             let mut key: String = String::new();
@@ -19,10 +34,17 @@ fn get_key() -> Option<String> {
             Some(key)
         }
         Err(e) => {
+            //This is probably overcomplicating it, but it's fine
             if e.kind().to_string() == "entity not found" {
                 //Key file doesn't exist
                 //Ask user to enter their key
-                ask_for_a_key()
+                let key = ask_for_a_key(Some("Key not found"))?;
+                let mut file = fs::OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .open(data_path).unwrap();
+                file.write_all(key.as_bytes()).unwrap();
+                Some(key)
             } else {
                 println!("{}", e);
                 None
@@ -33,6 +55,7 @@ fn get_key() -> Option<String> {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    get_key();
+    let key = get_key().unwrap();
+    println!("Key = {}",key);
     println!("args = {:#?}", args);
 }
